06a5d42f4c06da608c0d1df3ad6393d8d2fa9251
Josh <jkurz25@gmail.com>
2022-12-14 15:27:51 -0500

    adding ga analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-48759266-1"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
