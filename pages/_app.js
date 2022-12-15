6153e34a72ab3dce17075ccfbcc8a9c79cc8e45b
Josh <jkurz25@gmail.com>
2022-12-15 00:25:39 -0500

    adding GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-48759266-1"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
