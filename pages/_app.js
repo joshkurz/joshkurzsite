30966285f6967b74aed3f9947fe90b2ee4229f13
Josh <jkurz25@gmail.com>
2022-12-15 00:31:47 -0500

    fix GA

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import ReactGA from 'react-ga';
 const TRACKING_ID = "UA-48759266-1"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
-ReactGA.pageview(window.location.pathname);
+ReactGA.pageview("/");
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
